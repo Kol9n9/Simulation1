fe6777c6de3e012f029d73344261a6ed36eda720
NikolayS <slezenkonikolay@mail.ru>
2022-03-23 21:52:50 +0700

    init

---
+++ Simulation1/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace Simulation1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edHeight = new System.Windows.Forms.NumericUpDown();
+            this.edAngle = new System.Windows.Forms.NumericUpDown();
+            this.edSpeed = new System.Windows.Forms.NumericUpDown();
+            this.edWeight = new System.Windows.Forms.NumericUpDown();
+            this.edSize = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.edStepModel = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.startSimulate = new System.Windows.Forms.Button();
+            this.chartResult = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tableResults = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.clearData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edWeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edStepModel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartResult)).BeginInit();
+            this.tableResults.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Высота";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Угол";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Скорость";
+            // 
+            // edHeight
+            // 
+            this.edHeight.Location = new System.Drawing.Point(74, 9);
+            this.edHeight.Name = "edHeight";
+            this.edHeight.Size = new System.Drawing.Size(50, 20);
+            this.edHeight.TabIndex = 3;
+            // 
+            // edAngle
+            // 
+            this.edAngle.Location = new System.Drawing.Point(73, 35);
+            this.edAngle.Name = "edAngle";
+            this.edAngle.Size = new System.Drawing.Size(50, 20);
+            this.edAngle.TabIndex = 4;
+            this.edAngle.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // edSpeed
+            // 
+            this.edSpeed.Location = new System.Drawing.Point(73, 61);
+            this.edSpeed.Name = "edSpeed";
+            this.edSpeed.Size = new System.Drawing.Size(50, 20);
+            this.edSpeed.TabIndex = 5;
+            this.edSpeed.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // edWeight
+            // 
+            this.edWeight.DecimalPlaces = 2;
+            this.edWeight.Location = new System.Drawing.Point(185, 35);
+            this.edWeight.Name = "edWeight";
+            this.edWeight.Size = new System.Drawing.Size(50, 20);
+            this.edWeight.TabIndex = 9;
+            this.edWeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // edSize
+            // 
+            this.edSize.DecimalPlaces = 2;
+            this.edSize.Location = new System.Drawing.Point(186, 9);
+            this.edSize.Name = "edSize";
+            this.edSize.Size = new System.Drawing.Size(50, 20);
+            this.edSize.TabIndex = 8;
+            this.edSize.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(140, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Масса";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(134, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Размер";
+            // 
+            // edStepModel
+            // 
+            this.edStepModel.DecimalPlaces = 4;
+            this.edStepModel.Location = new System.Drawing.Point(375, 8);
+            this.edStepModel.Name = "edStepModel";
+            this.edStepModel.Size = new System.Drawing.Size(50, 20);
+            this.edStepModel.TabIndex = 11;
+            this.edStepModel.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(259, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Шаг моделирования";
+            // 
+            // startSimulate
+            // 
+            this.startSimulate.Location = new System.Drawing.Point(442, 6);
+            this.startSimulate.Name = "startSimulate";
+            this.startSimulate.Size = new System.Drawing.Size(75, 23);
+            this.startSimulate.TabIndex = 12;
+            this.startSimulate.Text = "Запуск";
+            this.startSimulate.UseVisualStyleBackColor = true;
+            this.startSimulate.Click += new System.EventHandler(this.startSimulate_Click);
+            // 
+            // chartResult
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartResult.ChartAreas.Add(chartArea2);
+            this.chartResult.Location = new System.Drawing.Point(15, 98);
+            this.chartResult.Name = "chartResult";
+            this.chartResult.Size = new System.Drawing.Size(903, 300);
+            this.chartResult.TabIndex = 13;
+            this.chartResult.Text = "chart1";
+            // 
+            // tableResults
+            // 
+            this.tableResults.ColumnCount = 2;
+            this.tableResults.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.tableResults.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableResults.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableResults.Location = new System.Drawing.Point(13, 405);
+            this.tableResults.Name = "tableResults";
+            this.tableResults.RowCount = 1;
+            this.tableResults.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableResults.Size = new System.Drawing.Size(905, 194);
+            this.tableResults.TabIndex = 14;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.label10, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label9, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label8, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label7, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(99, 188);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label10.AutoSize = true;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Location = new System.Drawing.Point(3, 141);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 47);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Скорость в последней точке";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Location = new System.Drawing.Point(3, 94);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 47);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Максимальная высота";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(3, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 47);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Расстояние";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(3, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 47);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Шаг моделирования";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // clearData
+            // 
+            this.clearData.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.clearData.Location = new System.Drawing.Point(442, 37);
+            this.clearData.Name = "clearData";
+            this.clearData.Size = new System.Drawing.Size(75, 23);
+            this.clearData.TabIndex = 15;
+            this.clearData.Text = "Очистить";
+            this.clearData.UseVisualStyleBackColor = true;
+            this.clearData.Click += new System.EventHandler(this.clearData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 611);
+            this.Controls.Add(this.clearData);
+            this.Controls.Add(this.tableResults);
+            this.Controls.Add(this.chartResult);
+            this.Controls.Add(this.startSimulate);
+            this.Controls.Add(this.edStepModel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.edWeight);
+            this.Controls.Add(this.edSize);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.edSpeed);
+            this.Controls.Add(this.edAngle);
+            this.Controls.Add(this.edHeight);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edWeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edStepModel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartResult)).EndInit();
+            this.tableResults.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown edHeight;
+        private System.Windows.Forms.NumericUpDown edAngle;
+        private System.Windows.Forms.NumericUpDown edSpeed;
+        private System.Windows.Forms.NumericUpDown edWeight;
+        private System.Windows.Forms.NumericUpDown edSize;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown edStepModel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button startSimulate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartResult;
+        private System.Windows.Forms.TableLayoutPanel tableResults;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button clearData;
+    }
+}
+
---
+++ Simulation1/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Simulation1
+{
+    public partial class Form1 : Form
+    {
+        private bool _isSimulated;
+        private double _stepModeling;
+        private double _speedX;
+        private double _speedY;
+        private double _time;
+        private double _currentX;
+        private double _currentY;
+        private double _maxY;
+        private double _k;
+        private const double _C = 0.15;
+        private const double _g = 9.81;
+        private const double _rho = 1.29;
+        private int _simulateCount = 0;
+        private const int _simulateMaxCount = 7;
+        private bool _isResizedChart;
+        public Form1()
+        {
+            InitializeComponent();
+            _isSimulated = false;
+            _isResizedChart = false;
+        }
+
+        private void startSimulate_Click(object sender, EventArgs e)
+        {
+            if (_isSimulated) return;
+            if(_simulateCount >= _simulateMaxCount)
+            {
+                MessageBox.Show($"Кол-во симуляций не может превышать {_simulateMaxCount} раз");
+                return;
+            }
+            #region parseValue
+            double height = (double)edHeight.Value;
+            double angle = (double)edAngle.Value;
+            double startSpeed = (double)edSpeed.Value;
+            double size = (double)edSize.Value;
+            double weight = (double)edWeight.Value;
+            _stepModeling = (double)edStepModel.Value;
+            #endregion
+
+            _time = 0;
+            _currentX = 0;
+            _maxY = _currentY = height;
+            angle = angle * Math.PI / 180;
+            double cosA = Math.Cos(angle);
+            double sinA = Math.Sin(angle);
+            _k = 0.5 * _C * _rho * size / weight;
+            _speedX = startSpeed * cosA;
+            _speedY = startSpeed * sinA;
+
+            CreateSeries();
+            
+            chartResult.Series.Last().Points.AddXY(_currentX,_currentY);
+            _isSimulated = true;
+            _simulateCount++;
+            timer1.Start();
+        }
+        private void ResizeChart()
+        {
+            float legendmaxsize = 140;
+
+            float maxpercentage = (legendmaxsize / (float)chartResult.Width) * 100;
+
+            var area = chartResult.ChartAreas.Last();
+
+            area.Position.Auto = false;
+
+            area.Position.Height = 100;
+            area.Position.Width = 100F - maxpercentage;
+
+            area.Position.Y = 0;
+            area.Position.X = maxpercentage;
+
+            _isResizedChart = true;
+        }
+        private void CreateSeries()
+        {
+            var series = chartResult.Series.Add($"Simulate {chartResult.Series.Count + 1}");
+            chartResult.Legends.Add(new System.Windows.Forms.DataVisualization.Charting.Legend($"Legend {chartResult.Series.Count + 1}"));
+            var legend = chartResult.Legends.Last();
+            legend.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+
+            if (!_isResizedChart) ResizeChart();
+            
+            chartResult.ChartAreas.Last().AxisX.Minimum = 0;
+            chartResult.ChartAreas.Last().AxisY.Minimum = 0;
+
+            series.Legend = legend.Name;
+            series.IsVisibleInLegend = true;
+            series.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double root = Math.Sqrt(_speedX * _speedX + _speedY * _speedY);
+            _time += _stepModeling;
+            _speedX -= _k * _speedX * root * _stepModeling;
+            _speedY -= (_g + _k * _speedY * root) * _stepModeling;
+
+            _currentX += _speedX * _stepModeling;
+            _currentY += _speedY * _stepModeling;
+
+            if(_currentY > _maxY) _maxY = _currentY;
+
+            chartResult.Series.Last().Points.AddXY(_currentX, _currentY);
+
+            if (_currentY <= 0)
+                StopTimer();
+
+        }
+        private void StopTimer()
+        {
+            timer1.Stop();
+            _isSimulated = false;
+
+            double speed = Math.Sqrt(_speedX * _speedX + _speedY * _speedY);
+
+            int columnCount = tableResults.ColumnCount += 1;
+            tableResults.Controls.Add(CreateResultLayout(_stepModeling.ToString(), _currentX.ToString(), _maxY.ToString(), speed.ToString()), columnCount-1, 0);
+        }
+
+        private TableLayoutPanel CreateResultLayout(string stepModeling, string distance, string maxHeight, string speedInEnd)
+        {
+            TableLayoutPanel result = new TableLayoutPanel();
+            result.ColumnCount = 1;
+            result.RowCount = 4;
+            result.Controls.Add(CreateResultLabel(stepModeling), 0, 0);
+            result.Controls.Add(CreateResultLabel(distance), 0, 1);
+            result.Controls.Add(CreateResultLabel(maxHeight), 0, 2);
+            result.Controls.Add(CreateResultLabel(speedInEnd), 0, 3);
+            result.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            result.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            result.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            result.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            result.Size = new Size(99, 188);
+            
+            return result;
+        }
+        private Label CreateResultLabel(string text)
+        {
+            Label label = new Label();
+            label.Anchor = AnchorStyles.Top | AnchorStyles.Bottom
+            | AnchorStyles.Left
+            | AnchorStyles.Right;
+            label.AutoSize = true;
+            label.BorderStyle = BorderStyle.FixedSingle;
+            label.TextAlign = ContentAlignment.MiddleLeft;
+            label.Text = text;
+            label.Margin = new Padding(0);
+            return label;
+        }
+        private void clearData_Click(object sender, EventArgs e)
+        {
+            if (_isSimulated)
+            {
+                MessageBox.Show("Во время симуляции, нельзя очищать данные");
+                return;
+            }
+            foreach(var serie in chartResult.Series)
+            {
+                serie.Points.Clear();
+            }
+            chartResult.Series.Clear();
+            chartResult.Legends.Clear();
+            _simulateCount = 0;
+            _isResizedChart = false;
+        }
+    }
+}
